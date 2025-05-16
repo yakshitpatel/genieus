eff8cfd9e79882a31ed9529dbdb185d2f9d58960
Yakshit Patel <68852135+yakshitdesign@users.noreply.github.com>
2025-05-16 13:27:05 +0530

    tag-manager

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -68,6 +68,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NWJ7GLFR');`}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Schema.org JSON-LD */}
         <script type="application/ld+json">
           {JSON.stringify({
@@ -106,6 +115,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWJ7GLFR"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
